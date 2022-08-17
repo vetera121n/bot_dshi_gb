f969db6cbb4d220681a6113905054be5567dcaf9
Vladislav <vetera121n@gmail.com>
2022-08-17 15:10:40 +0500

    не понимать

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
-async def process_start_command(message: type.Message):
+async def process_start_command(message: types.Message):
     await message.reply('Привет!\nНапиши мне что-нибудь!')
 
 @dp.message_handler(commands=['help'])
@@ -18,8 +18,8 @@ async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
 
 @dp.message_handler()
-async def echo_massage(msg: types.Message):
-    await bot.send_massage(msg.from_user.id, msg.text)
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
